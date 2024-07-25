9dd255874e075860f9933c73808fcfd3c1f9371f
murodjon72 <muro21601@gmail.com>
2024-07-25 08:10:53 +0500

    som  updates

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 let arr  = [4, 16, 19, 22, 11, 144, 967, 19124];
-let result = arr.filter((item) => item % 2 === 0 );
-console.log(result);
+let even = arr.filter((item) => item % 2 === 0 );
+console.log(even);
+
+let odd = arr.filter((item) => item % 2 ===  1)
+console.log(odd);
